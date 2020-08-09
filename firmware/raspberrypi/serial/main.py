ec0c50cf922e4fecb00498bafda82634ea20be01
leejunseo <junseo010234@gmail.com>
2020-08-09 23:05:03 +0900

    add code 'change json'

--- firmware/raspberrypi/serial/main.py
+++ firmware/raspberrypi/serial/main.py
@@ -1,18 +1,15 @@
 import serial
 import json
 
-a = {1: 'false'}
+json_D= {'0': 0}
 ser = serial.Serial(port = "com21", baudrate = 9600)
-
-
-
 
 if __name__ == "__main__":
     while 1:
         if ser.readable():
             res = ser.readline()
-            #a = res.find('((')
-
-        
-            print(res)
+        for i in range(1,6):
+            json_D[i] = int(res.split(' ')[0])
+        ptc = json.dumps(json_D)
+        print(ptc)
 

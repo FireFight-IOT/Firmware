cb9c7464405ea0f752b35efa478f826ffadd87c1
leejunseo <junseo010234@gmail.com>
2020-08-10 13:02:37 +0900

    fix error

--- firmware/raspberrypi/serial/main.py
+++ firmware/raspberrypi/serial/main.py
@@ -1,15 +1,19 @@
 import serial
 import json
 
-json_D= {'0': 0}
+json_D = {'0': 0}
 ser = serial.Serial(port = "com21", baudrate = 9600)
 
 if __name__ == "__main__":
+    json_D.clear()
     while 1:
         if ser.readable():
             res = ser.readline()
-        for i in range(1,6):
-            json_D[i] = int(res.split(' ')[0])
+            res = res[0:9]
+            res = res.decode()[:len(res)]
+        for i in range(0,5):
+            a = i + 1
+            json_D[a] = str(res).split(' ')[i]
         ptc = json.dumps(json_D)
         print(ptc)
 
